31dfd26cdc0d35646de890be1e77d763e276870e
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-11-15 18:25:46 +0100

    refactoring template

--- src/app.py
+++ src/app.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template
+from typing import Any
 
 app = Flask(__name__)
 
 
+def wrap_template_rendering(template_name: str, **context: Any):
+    return render_template(template_name, _author="Michael", context=context)
+    
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    # return render_template("home.html", _author="Michael")
+    # return wrap_template_rendering("home.html", _author="Michael")
+    return wrap_template_rendering("home.html")
 
 
 @app.route("/dices")
 def handle_dices():
-    return "Look at these beautiful dices"
+    # return "Look at these beautiful dices"
+    return wrap_template_rendering("dice.html")
 
 
 @app.route("/hello")
 @app.route("/hello/<string:name>")
 def hello_world(name: str = None):
-    return render_template("hello.html", _name=name)
+    # return render_template("hello.html", _name=name)
+    return wrap_template_rendering("hello.html", _name=name)
